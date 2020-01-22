c9bf4d34f6a12e1ef3f89031cf13b0bfe3e8978d
Kundan Kadam <kundanx.kadam@intel.com>
2020-01-22 10:55:56 +0530

    EtcdUI: enable reverse proxy to use it with https.
    Changes:
    1. Added configuration files for nginx in DEV and PROD mode.
    2. Made changes in start_etcdkeeper.py script to integrate nginx.
    3. Change in docker-compose.yml to add NGINX_HOST as env variable.
    
    Signed-off-by: Kundan Kadam <kundanx.kadam@intel.com>

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,61 @@
-FROM python:3.7-slim
-RUN  apt update && \
-     apt install -y curl unzip
+# Copyright (c) 2020 Intel Corporation.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+ARG EIS_VERSION
+FROM ia_eisbase:$EIS_VERSION as eisbase
 ARG ETCD_KEEPER_VERSION
-RUN curl -L https://github.com/evildecay/etcdkeeper/releases/download/${ETCD_KEEPER_VERSION}/etcdkeeper-${ETCD_KEEPER_VERSION}-linux_x86_64.zip -o etcdkeeper-${ETCD_KEEPER_VERSION}-linux_x86_64.zip && \
-    unzip etcdkeeper-${ETCD_KEEPER_VERSION}-linux_x86_64.zip
-RUN rm -rf etcdkeeper-${ETCD_KEEPER_VERSION}-linux_x86_64.zip
-RUN chmod +x etcdkeeper/etcdkeeper
+
+RUN apt update && \
+    apt install -y curl unzip && \
+    curl -L https://github.com/evildecay/etcdkeeper/releases/download/${ETCD_KEEPER_VERSION}/etcdkeeper-${ETCD_KEEPER_VERSION}-linux_x86_64.zip -o etcdkeeper-${ETCD_KEEPER_VERSION}-linux_x86_64.zip && \
+    unzip etcdkeeper-${ETCD_KEEPER_VERSION}-linux_x86_64.zip && \
+    rm -rf etcdkeeper-${ETCD_KEEPER_VERSION}-linux_x86_64.zip && \
+    chmod +x etcdkeeper/etcdkeeper
+
+RUN apt-get update && \
+    apt-get install -y nginx && \
+    apt-get update && apt-get install -y procps
+
+ARG EIS_UID
+RUN chown -R ${EIS_UID}:${EIS_UID} /var/log/nginx/ && \
+    chown -R ${EIS_UID}:${EIS_UID} /var/lib/nginx/
+
+
+FROM ia_common:$EIS_VERSION as common
+
+FROM eisbase
+
+COPY --from=common ${GO_WORK_DIR}/common/libs ${PY_WORK_DIR}/libs
+COPY --from=common ${GO_WORK_DIR}/common/util ${PY_WORK_DIR}/util
+
+COPY nginx.conf /etc/nginx/nginx.conf
 COPY start_etcdkeeper.py ./
+COPY eis_nginx_prod.conf ./
+COPY eis_nginx_dev.conf ./
+
+RUN touch /run/nginx.pid
+
+ARG EIS_UID
+RUN chown -R ${EIS_UID}:${EIS_UID} /run/nginx.pid && \
+    ln -sf /dev/stdout /var/log/nginx/access.log && ln -sf /dev/stderr /var/log/nginx/error.log && \
+    rm -rf /var/lib/nginx && ln -sf /tmp/nginx /var/lib/nginx && \
+    rm -f /etc/nginx/sites-enabled/default
+
 ENTRYPOINT ["python3", "start_etcdkeeper.py"]
--- start_etcdkeeper.py
+++ start_etcdkeeper.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Copyright (c) 2019 Intel Corporation.
+# Copyright (c) 2020 Intel Corporation.
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -17,12 +17,23 @@
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.#!/usr/bin/python3
+# SOFTWARE.
 
 import os
 import subprocess
 from distutils.util import strtobool
 import logging
+import subprocess
+from libs.ConfigManager import ConfigManager
+from util.util import Util
+import shutil
+import threading
+
+
+def _execute_cmd(cmd):
+    cmd_output = subprocess.check_output(cmd, shell=True)
+    return cmd_output
+
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
@@ -30,17 +41,61 @@ if __name__ == "__main__":
         devMode = bool(strtobool(os.environ['DEV_MODE']))
     except KeyError:
         devMode = 1
+
+    if not devMode:
+        app_name = os.environ["AppName"]
+        conf = Util.get_crypto_dict("root")
+        cfg_mgr = ConfigManager()
+        config_client = cfg_mgr.get_config_client("etcd", conf)
+
+        server_cert = config_client.GetConfig("/" + app_name + "/server_cert")
+        server_key = config_client.GetConfig("/" + app_name + "/server_key")
+
+        with open('/tmp/nginx/server_cert.pem', 'w') as f:
+            f.write(server_cert)
+
+        with open('/tmp/nginx/server_key.pem', 'w') as f:
+            f.write(server_key)
+
+    cmd = _execute_cmd("hostname -I | awk '{print $1}'")
+    ip = str(cmd, encoding='utf-8').strip()
+
+    port = os.environ["NGINX_PORT"]
+
+    if devMode:
+        cmd1 = "sed  's/NGINX_HOST/{}/' \
+                ./eis_nginx_dev.conf > \
+                /tmp/nginx/eis_nginx_temp.conf".format(ip)
+        cmd2 = "sed  's/NGINX_PORT/{}/' \
+                /tmp/nginx/eis_nginx_temp.conf \
+                >/tmp/nginx/eis_nginx.conf".format(port)
+        _execute_cmd(cmd1)
+        _execute_cmd(cmd2)
+    else:
+        cmd1 = "sed  's/NGINX_HOST/{}/' \
+                ./eis_nginx_prod.conf >/ \
+                tmp/nginx/eis_nginx_temp.conf".format(ip)
+        cmd2 = "sed  's/NGINX_PORT/{}/' \
+                /tmp/nginx/eis_nginx_temp.conf \
+                >/tmp/nginx/eis_nginx.conf".format(port)
+        _execute_cmd(cmd1)
+        _execute_cmd(cmd2)
+
     try:
         if devMode:
-            subprocess.run(["./etcdkeeper/etcdkeeper", "-p", "7070"])
+            subprocess.Popen(["./etcdkeeper/etcdkeeper",
+                              "-p", "7070"], start_new_session=True)
+
         else:
-            subprocess.run(["etcdkeeper/etcdkeeper",
-                            "-h", "127.0.0.1",
-                            "-p", "7070",
-                            "-usetls",
-                            "-cacert", "/run/secrets/ca_etcd",
-                            "-key", "/run/secrets/etcd_root_key",
-                            "-cert", "/run/secrets/etcd_root_cert",
-                            "-auth"])
+            subprocess.Popen(["./etcdkeeper/etcdkeeper",
+                              "-h", "127.0.0.1",
+                              "-p", "7070",
+                              "-usetls",
+                              "-cacert", "/run/secrets/ca_etcd",
+                              "-key", "/run/secrets/etcd_root_key",
+                              "-cert", "/run/secrets/etcd_root_cert",
+                              "-auth"], start_new_session=True)
+
     except Exception as e:
         logger.exception("etcdkeeper start exception:{}".format(e))
+    _execute_cmd("nginx")
